6df65abe59192f8809bd79df81cf184773ec4c5c
WilsonAlmonte <wilson.a.almonte31@gmail.com>
2025-02-07 19:28:21 -0400

    Add CharacterSelectionWrapper component for character selection screen

---
+++ src/presentation/home/character-selection/character-selection-wrapper.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+export const CharacterSelectionWrapper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      id="character-selection"
+      className="flex flex-col items-center justify-center p-10 min-h-screen"
+    >
+      <Image
+        src="/line.png"
+        width={300}
+        height={31.25}
+        alt="line"
+        className="mb-10"
+      />
+      <div className="max-w-5xl text-center">
+        <h1 className="text-5xl font-bold font-elements text-bright mb-5">
+          Choose your Character <br />
+        </h1>
+
+        <p className="text-2xl font-body text-base-100">
+          Step into a world where your choices shape the story. Unleash your
+          imagination, explore enchanted lands, and meet unforgettable
+          characters. Whether you&apos;re a hero, a rogue, or a dreamer, every
+          decision leads to a tale uniquely yours. Begin your journey and let
+          the story unfold—your story awaits!
+        </p>
+      </div>
+      {children}
+    </div>
+  );
+};
